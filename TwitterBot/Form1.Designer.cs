a9086cf25f056bc04b0b1d1c49c83c70f2fedc4d
Hayden Carlson <haydog2@hotmail.com>
2017-07-04 22:15:03 -0700

    made method for http request. created basic form

---
+++ TwitterBot/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TwitterBot {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.startBot = new System.Windows.Forms.Button();
+            this.accountCreatorTimer = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.accountsList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // startBot
+            // 
+            this.startBot.Location = new System.Drawing.Point(23, 320);
+            this.startBot.Name = "startBot";
+            this.startBot.Size = new System.Drawing.Size(182, 56);
+            this.startBot.TabIndex = 0;
+            this.startBot.Text = "Start Creating";
+            this.startBot.UseVisualStyleBackColor = true;
+            this.startBot.Click += new System.EventHandler(this.startBot_Click);
+            // 
+            // accountCreatorTimer
+            // 
+            this.accountCreatorTimer.Tick += new System.EventHandler(this.accountCreatorTimer_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 290);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Password for Accounts";
+            // 
+            // accountsList
+            // 
+            this.accountsList.FormattingEnabled = true;
+            this.accountsList.Location = new System.Drawing.Point(23, 12);
+            this.accountsList.Name = "accountsList";
+            this.accountsList.Size = new System.Drawing.Size(182, 225);
+            this.accountsList.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 387);
+            this.Controls.Add(this.accountsList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.startBot);
+            this.Name = "Form1";
+            this.Text = "TwitterBot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBot;
+        private System.Windows.Forms.Timer accountCreatorTimer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox accountsList;
+    }
+}
+
---
+++ TwitterBot/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace TwitterBot {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void startBot_Click(Object sender, EventArgs e) {
+            accountCreatorTimer.Start();
+        }
+
+        private void accountCreatorTimer_Tick(Object sender, EventArgs e) {
+
+        }
+        
+        async static void makeRequest(string url) {
+            using (HttpClient client = new HttpClient()) {
+                HttpResponseMessage response = await client.PostAsync(url);
+                using (HttpContent content = response.Content) {
+
+                }
+            }
+
+
+        }
+
+    }
+}
