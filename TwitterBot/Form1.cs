e310a303373594ef07feb0c7864c577681eb871a
Hayden Carlson <haydog2@hotmail.com>
2017-07-05 22:47:14 -0700

    Added CookieAwareWebClient class. Created cookiejar to store authenticity token

--- TwitterBot/Form1.cs
+++ TwitterBot/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
 using System.Net.Http;
 using System.Diagnostics;
@@ -21,16 +22,24 @@ namespace TwitterBot {
                 string emailAddress = webBrowser1.Document.GetElementById("mail").GetAttribute("value");
                 return emailAddress;
             }
-            return "nothing";
+            return "";
         }
-
+        
         private void startBot_Click(Object sender, EventArgs e) {
             string email = getEmail();
-            Debug.WriteLine(email);
+            if (email != String.Empty) {
+
+            }
         }
 
         private void accountCreatorTimer_Tick(Object sender, EventArgs e) {
 
+        }
+        
+        public void makeRequest() {
+            
+            var cookieJar = new CookieContainer();
+            CookieAwareWebClient client = new CookieAwareWebClient(cookieJar);
         }
 
     }
